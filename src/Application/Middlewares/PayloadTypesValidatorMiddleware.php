1313ecc98bca7bde9e25f389f8ec32b966dc2a3e
corbocal <monkey-factory@protonmail.com>
2025-05-25 21:58:45 +0200

    payload validator

---
+++ src/Application/Middlewares/PayloadTypesValidatorMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Corbocal\EasySlim\Middlewares;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class PayloadTypesValidatorMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        protected readonly string $configFile,
+        protected readonly string $configIndex
+    ) {
+    }
+
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        $payload = $request->getParsedBody();
+        return $handler->handle($request);
+    }
+}
